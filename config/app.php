0181c4e0c0d75a8b1bf704b0b8b3ad6665759cb0
dav3sk <dav3sk@hotmail.com>
2019-07-29 21:29:04 -0300

    Add AdminLTE template

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * AdminLTE Service Provider
+         */
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
     ],
 
     /*
