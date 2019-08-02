6fb48fcbdbfcd33a01b6280c3a4ff8b376de5e41
dav3sk <dav3sk@hotmail.com>
2019-08-02 11:04:01 -0300

    Add create method on FavoriteController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -4,17 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Favorite;
+use App\Http\Requests\FavoriteStoreRequest;
 use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
-    public function show () {
+    public function index ()
+    {
         $favorites = Favorite::where('user_id', Auth::user()->id)->get();
 
         return view('user\favorites', compact('favorites'));
     }
 
-    public function delete (Favorite $favorite) {
+    public function create (FavoriteStoreRequest $request)
+    {
+        $article = new Favorite([
+            'user_id' => Auth::user()->id,
+            'title' => $request->input('title'),
+            'url_image' => $request->input('url_image'),
+            'url' => $request->input('url'),
+            'date' => date('Y-m-d H:i:s', strtotime($request->input('date')))
+        ]);
+
+        $article->saveOrFail();
+    }
+
+    public function delete (Favorite $favorite)
+    {
         $favorite->delete();
     }
 }
