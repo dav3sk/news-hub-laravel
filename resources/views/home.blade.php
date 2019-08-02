5aaf9698457219fc22abda11f82a2a5635c8d3e2
dav3sk <dav3sk@hotmail.com>
2019-08-02 11:19:34 -0300

    Modified home page to show articles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,5 +22,104 @@
 @stop
 
 @section('content')
-    <p>Você está conectado!</p>
+<div class="box box-primary">
+    <div class="box-body">
+        <table class="table table-bordered" style="text-align: center">
+            <tbody>
+                @if (count($news) > 0)
+                <tr>
+                    <th>
+                        Imagem
+                    </td>
+                    <th>
+                        Título
+                    </th>
+                    <th>
+                        Data
+                    </th>
+                </tr>
+                @foreach ($news as $new)
+                <tr>
+                    <td>
+                        <img src={{$new['urlToImage']}} width="150">
+                    </td>
+                    <td>
+                        <a href={{$new['url']}} target="_blank">
+                            <h3> {{$new['title']}} </h3>
+                        </a>
+                    </td>
+                    <td>
+                        {{date('d/m/Y', strtotime($new['publishedAt']))}}
+                    </td>
+                    <td>
+                    <a class="btn btn-xs add-favorite" data-new="{{json_encode($new)}}">
+                            <i class="fa fa-plus" style="font-size: 60px;"></i>
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+                @else
+                    Nenhum artigo encontrado...
+                @endif
+            </tbody>
+        </table>
+    </div>
+    <div class="box-footer" style="text-align:center">
+        Powered by <a href="https://newsapi.org/">News API.</a>
+    </div>
+</div>
 @stop
+
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script>
+        $('.add-favorite').on('click', function () {
+            $(this).attr("disabled", true);
+            var article = $(this).data('new');
+
+            $.ajax({
+                url: '{{ route('favorite.create') }}',
+                method: 'POST',
+                data: {
+                    'title': article.title,
+                    'url': article.url,
+                    'url_image': article.urlToImage,
+                    'date': article.publishedAt,
+                },
+
+                success: function (xhr) {
+                    Swal.fire({
+                        title: 'Artigo adicionado aos favoritos!',
+                        type: 'success',
+                        customClass: 'swal-big-font',
+                    })
+                    window.location.reload();
+                },
+                error: function (xhr) {
+                    console.log(xhr);
+                    Swal.fire({
+                        title: 'Algo deu errado!',
+                        text: 'Não foi possível adicionar o artigo aos favoritos.',
+                        type: 'error',
+                        customClass: 'swal-big-font',
+                    })
+                    $('.add-favorite').attr("disabled", false);
+                }
+            });
+        })
+    </script>
+@endsection
+
+@section('css')
+    <style>
+        .swal-big-font {
+            font-size: 0.9em !important;
+        }
+        td {
+            vertical-align: middle !important;
+        }
+        th {
+            text-align: center !important;
+        }
+    </style>
+@endsection
