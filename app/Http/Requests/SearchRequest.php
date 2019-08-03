474054899f820906184730c59e8d5ee739be25da
dav3sk <dav3sk@hotmail.com>
2019-08-03 12:30:00 -0300

    Add SearchRequest

---
+++ app/Http/Requests/SearchRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'keyword'   => 'required|string',
+            'category'   => 'string',
+            'date_start' => 'date',
+            'date_end'   => 'date',
+        ];
+    }
+
+    public function filters()
+    {
+        return [
+            'keyword'    => 'trim|escape',
+            'category'   => 'trim|escape',
+            'date_start' => 'trim|escape',
+            'date_end'   => 'trim|escape',
+        ];
+    }
+}
