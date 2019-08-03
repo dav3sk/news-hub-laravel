447291b4b03b478681d28fcb0dc4333ff8a59533
dav3sk <dav3sk@hotmail.com>
2019-08-03 12:30:15 -0300

    Add search method for HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use App\Http\Requests\SearchRequest;
 
 class HomeController extends Controller
 {
-    private $client;
 
     /**
      * Create a new controller instance.
@@ -16,8 +16,6 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-
-        $this->client = new Client();
     }
 
     /**
@@ -27,11 +25,33 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $request = $this->client->get("https://newsapi.org/v2/top-headlines?q=Apple&apiKey=305b29bdbd95457db87f67ad2c9d1c3d");
-        // TODO: check if request is valid
-        $response = json_decode($request->getBody(), true);
+        return view('home');
+    }
 
-        $news = $response['articles'];
+    public static function search(SearchRequest $request)
+    {
+        $request_url = "https://newsapi.org/v2/everything?pageSize=30&language=pt&";
+        $request_url .= "q=" . $request->keyword . "&";
+
+        if ($request->has('date_start')) {
+            $request_url .= "from=" . $request->date_start . "&";
+        }
+        if ($request->has('date_end')) {
+            $request_url .= "to=" . $request->date_end . "&";
+        }
+
+        try {
+            $response = (new Client())->get($request_url . "ApiKey=305b29bdbd95457db87f67ad2c9d1c3d");
+        } catch (\Exception $e) {
+            return redirect()->route('home')->with('error', 'Algo deu errado! Verifique se o intervalo de tempo não é muito grande.');
+        }
+
+        $body = json_decode($response->getBody(), true);
+        if ($body['totalResults'] == 0) {
+            return redirect()->route('home')->with('info', 'Nenhum resultado encontrado.');
+        }
+
+        $news = $body['articles'];
 
         return view('home', compact('news'));
     }
