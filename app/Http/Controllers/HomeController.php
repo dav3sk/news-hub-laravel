bd8a4b2d8cddd1b2b15c33a021b37580495a8039
dav3sk <dav3sk@hotmail.com>
2019-08-04 03:37:28 -0300

    Modified search method to use separate function for request building

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\SearchRequest;
 
 class HomeController extends Controller
 {
+    private $api_url = "https://newsapi.org/v2/everything?pageSize=30&language=pt&";
+    private $api_key = "ApiKey=305b29bdbd95457db87f67ad2c9d1c3d";
 
     /**
      * Create a new controller instance.
@@ -28,31 +30,39 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public static function search(SearchRequest $request)
+    public function search(SearchRequest $request)
     {
-        $request_url = "https://newsapi.org/v2/everything?pageSize=30&language=pt&";
-        $request_url .= "q=" . $request->keyword . "&";
-
-        if ($request->has('date_start')) {
-            $request_url .= "from=" . $request->date_start . "&";
-        }
-        if ($request->has('date_end')) {
-            $request_url .= "to=" . $request->date_end . "&";
-        }
+        $request_url = $this->build_request($request);
 
         try {
-            $response = (new Client())->get($request_url . "ApiKey=305b29bdbd95457db87f67ad2c9d1c3d");
+            $response = (new Client())->get($request_url);
         } catch (\Exception $e) {
             return redirect()->route('home')->with('error', 'Algo deu errado! Verifique se o intervalo de tempo não é muito grande.');
         }
 
         $body = json_decode($response->getBody(), true);
+
         if ($body['totalResults'] == 0) {
             return redirect()->route('home')->with('info', 'Nenhum resultado encontrado.');
         }
 
-        $news = $body['articles'];
+        $news = collect($body['articles']);
 
         return view('home', compact('news'));
+    }
+
+    private function build_request(SearchRequest $request)
+    {
+        $url = $this->api_url;
+        $url .= "q=" . $request->keyword . "&";
+
+        if ($request->has('date_start')) {
+            $url .= "from=" . $request->date_start . "&";
+        }
+        if ($request->has('date_end')) {
+            $url .= "to=" . $request->date_end . "&";
+        }
+
+        return $url . $this->api_key;
     }
 }
